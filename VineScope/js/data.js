c1c23e39b7ed11d466e83055d8005acb84cf518e
d.georgiev.91@gmail.com <d.georgiev.91@gmail.com>
2013-08-31 15:42:14 +0300

    Added some content to homepage

---
+++ VineScope/js/data.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="//Microsoft.WinJS.1.0/js/ui.js" />
+(function () {
+    "use strict";
+
+    var vines = new WinJS.Binding.List([{ pesho: "pesho" }]);
+
+    WinJS.Namespace.define("Data", {
+        vines: vines,
+    });
+})();
---
+++ VineScope/js/httpRequester.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="//Microsoft.WinJS.1.0/js/base.js" />
+(function () {
+    'use strict';
+
+    var xhrRequesterPromise = function (url, type, data) {
+        if (data) {
+            data = JSON.stringify(data);
+        }
+
+        var promise = 
+            WinJS.xhr({
+                url: url,
+                type: type,
+                data: data
+            })
+
+        return promise;
+    }
+
+    function postJson(url, data) {
+        return xhrRequesterPromise(url, "post", data);
+    }
+
+    function getJson(url) {
+        return xhrRequesterPromise(url, "get");
+    }
+
+    WinJS.Namespace.define("HttpRequester", {
+        getJson: getJson,
+        postJson: postJson,
+    });
+})();
